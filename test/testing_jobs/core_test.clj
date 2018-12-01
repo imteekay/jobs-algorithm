4a5e5a7c93bb848e645c5dad1b3d03fa743a69ac
TK <leandrotk100@gmail.com>
2018-11-30 23:33:16 -0200

    Add one more unit test to prioritize function

--- test/testing_jobs/core_test.clj
+++ test/testing_jobs/core_test.clj
@@ -20,4 +20,8 @@
 
      [{:id "f26e890b-df8e-422e-a39c-7762aa0bac36" :type "rewards-question" :urgent false}
       {:id "690de6bc-163c-4345-bf6f-25dd0c58e864" :type "bills-questions" :urgent false}
-      {:id "c0033410-981c-428a-954a-35dec05ef1d2" :type "bills-questions" :urgent false}])))
+      {:id "c0033410-981c-428a-954a-35dec05ef1d2" :type "bills-questions" :urgent false}])
+
+    (is
+     (prioritize [])
+     [])))
