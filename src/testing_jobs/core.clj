86ed839c26d15b404abc45fc4821e54f32f976d4
TK <leandrotk100@gmail.com>
2018-12-04 23:43:04 -0200

    Removing tests code from src

--- src/testing_jobs/core.clj
+++ src/testing_jobs/core.clj
@@ -1,29 +1,9 @@
 (ns testing-jobs.core)
 
-;; -- jobs and agents definition --
-(def jobs
-  [{:id "f26e890b-df8e-422e-a39c-7762aa0bac36" :type "rewards-question" :urgent false}
-   {:id "690de6bc-163c-4345-bf6f-25dd0c58e864" :type "bills-questions" :urgent false}
-   {:id "c0033410-981c-428a-954a-35dec05ef1d2" :type "bills-questions" :urgent true}])
-
-(def agents
-  [{:id "8ab86c18-3fae-4804-bfd9-c3d6e8f66260"
-    :name "BoJack Horseman"
-    :primary_skillset ["bills-questions"]
-    :secondary_skillset []}
-   {:id "ed0e23ef-6c2b-430c-9b90-cd4f1ff74c88"
-    :name "Mr. Peanut Butter"
-    :primary_skillset ["rewards-question"]
-    :secondary_skillset ["bills-questions"]}])
-;; -- end of jobs and agents definition --
-
-
 ;; -- sorting by urgency --
 (defn prioritize
   [jobs]
   (sort-by (complement :urgent) jobs))
-
-(prioritize jobs)
 ;; -- end of sorting by urgency --
 
 
@@ -116,6 +96,7 @@
        (first)
        (assignments assigned-jobs job)))
 
+
 ;; ----- Testing sorted agents -----
 (defn run-assignment [agents jobs assigned-jobs]
   (loop
@@ -132,6 +113,23 @@
          (assign-agent agents (first jobs) assigned-jobs)))
 
       assigned-jobs)))
+
+;; -- jobs and agents definition --
+(def jobs
+  [{:id "f26e890b-df8e-422e-a39c-7762aa0bac36" :type "rewards-question" :urgent false}
+   {:id "690de6bc-163c-4345-bf6f-25dd0c58e864" :type "bills-questions" :urgent false}
+   {:id "c0033410-981c-428a-954a-35dec05ef1d2" :type "bills-questions" :urgent true}])
+
+(def agents
+  [{:id "8ab86c18-3fae-4804-bfd9-c3d6e8f66260"
+    :name "BoJack Horseman"
+    :primary_skillset ["bills-questions"]
+    :secondary_skillset []}
+   {:id "ed0e23ef-6c2b-430c-9b90-cd4f1ff74c88"
+    :name "Mr. Peanut Butter"
+    :primary_skillset ["rewards-question"]
+    :secondary_skillset ["bills-questions"]}])
+;; -- end of jobs and agents definition --
 
 (defn -main [& args]
   (println (run-assignment agents jobs [])))
